0f8880162c625611438353f10d2a4630930e433a
Victor Arowo <arowov@gmail.com>
2019-08-22 13:50:56 +0100

    Refactored Prototypes to Use Class Keyword

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,111 @@
-/* 
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.stomach = [];
+    }
+    greet() {
+        return `Name - ${this.name} Age - ${this.age}`;
+    }
+    eatEdibles(edible) {
+        this.stomach.push(edible);
+    }
+    poop() {
+        this.stomach.length = 0;
+    }
+}
 
-Prototype Refactor
+class Car {
+    constructor(name, make) {
+        this.name = name;
+        this.make = make;
+        this.odometer = 0;
+        this.isCrashed = false;
+    }
+    drive(distance) {
+        if (!this.isCrashed) {
+            this.odometer += distance;
+        }
+        else {
+            return this.odometer;
+        }
+    }
+    crash() {
+        this.isCrashed = true;
+    }
+    repair() {
+        this.isCrashed = false;
+    }
+}
 
-1. Copy and paste your code or the solution from yesterday
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+class Baby extends Person {
+    play() {
+        return `This is how a baby plays`;;
+    }
+}
 
-*/
+
+class Employee{
+    constructor(data){
+        this.id = data.id;
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.position = data.position;
+        this.office = data.office;
+        this.isManager = false;
+    }
+
+    view(){
+        return `Employee with ID - ${this.id} is named ${this.firstName} ${this.lastName}`
+    }
+
+    promote(){
+        this.isManager = true;
+    }
+
+    switchOffices(office){
+        this.office = office;
+    }
+    
+}
+
+
+//Stretch
+
+class GameObject {
+    constructor(props) {
+        this.createdAt = props.createdAt;
+        this.name = props.name;
+        this.dimensions = props.dimensions;
+    }
+
+    destroy() {
+        return `${this.name} was removed from the game.`
+    }
+}
+
+
+class CharacterStats extends GameObject {
+    constructor(props) {
+        this.healthPoints = props.healthPoints;
+    }
+
+    takeDamage() {
+        return `${this.name} took damage.`
+    }
+}
+
+
+class Humanoid extends CharacterStats{
+    constructor(props){
+        this.team = props.team;
+        this.weapons = props.weapons;
+        this.language = props.language;
+    }
+
+    greet(){
+        return `${this.name} offers a greeting in ${this.language}`
+    } 
+}
+  
